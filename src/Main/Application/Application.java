0b9f7ea71115b42cb4abcc0c4ba19c47413bea47
Kauan Araujo <araujokauan664@gmail.com>
2025-09-14 16:38:23 -0300

    atualização

--- src/Main/Application/Application.java
+++ src/Main/Application/Application.java
@@ -6,7 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         // inicialização
-
+        //atualização
         JanelaPrincipal janela1 = new JanelaPrincipal();
         janela1.setVisible(true);
 

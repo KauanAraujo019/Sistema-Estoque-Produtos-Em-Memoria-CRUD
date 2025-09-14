6ae3b753650a97acd20575a4541b841c9ed15e7b
Kauan Araujo <araujokauan664@gmail.com>
2025-09-14 16:25:10 -0300

    mudanca

--- ProjetoCadastroProdutos/src/Main/Application/Application.java
+++ ProjetoCadastroProdutos/src/Main/Application/Application.java
@@ -5,6 +5,7 @@ import Main.Application.Entities.Janelas.JanelaPrincipal;
 public class Application {
 
     public static void main(String[] args) {
+        // inicialização
 
         JanelaPrincipal janela1 = new JanelaPrincipal();
         janela1.setVisible(true);
